19001d9d59466e1f874a33b70b2073d8146278e1
bruno.gois <brunovgois@gmail.com>
2019-02-23 22:51:14 -0300

    projeto exemplo react native

---
+++ React-Native/albums/App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React from 'react';
+import { View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => (
+  <View style={{ flex: 1 }}>
+    <Header headerText={'Albums!'} />  
+    <AlbumList />
+  </View>
+  
+);
+
+
+export default App;
